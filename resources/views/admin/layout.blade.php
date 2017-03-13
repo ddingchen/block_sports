cf9d2be95431f136b841c0677ce0020af5a9aca4
Chen Ding <dc@ChendeMacBook-Pro.local>
2017-03-13 13:58:07 +0800

    修正了菜单上“报名列表”项和“线下报名录入”项的高亮冲突的问题。

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -24,7 +24,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li @if(preg_match('/\/admin\/ticket/' ,url()->current())) class="active" @endif><a href="/admin/ticket">惠山街道报名列表</a></li>
+            <li @if(preg_match('/\/admin\/ticket$/' ,url()->current())) class="active" @endif><a href="/admin/ticket">惠山街道报名列表</a></li>
             <li @if(preg_match('/\/admin\/ticket\/create/' ,url()->current())) class="active" @endif><a href="/admin/ticket/create">线下报名录入</a></li>
             <li @if(preg_match('/\/admin\/block/' ,url()->current())) class="active" @endif><a href="/admin/block">社区管理</a></li>
             <li @if(preg_match('/\/admin\/area/' ,url()->current())) class="active" @endif><a href="/admin/area">小区管理</a></li>
