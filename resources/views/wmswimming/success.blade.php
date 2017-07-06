4fdfa5f77a67c65d5e44f2f2b74ed2108f70792f
Chen Ding <183488175@qq.com>
2017-07-06 10:39:37 +0800

    增加了报名成功页面，赛事规程的链接跳转。

--- resources/views/wmswimming/success.blade.php
+++ resources/views/wmswimming/success.blade.php
@@ -9,7 +9,7 @@
 	            <h2 class="weui-msg__title">报名成功</h2>
 	            <p class="weui-msg__desc">
 	            	每个身份证号最多可以报名两个项目<br/>
-	            	更多比赛信息可以查看<a href="javascript:void(0);">比赛规程</a>
+	            	更多比赛信息可以查看<a href="http://mp.weixin.qq.com/s?__biz=MzAwNTAyNzEwOQ==&mid=503151796&idx=1&sn=8fb6cbde5a2d543828b9e13c02d72dcf&chksm=032b38fe345cb1e860d1e2260dc713e9fa4b8b239281f2798dbae5157c6a18579c8e12b34db7">比赛规程</a>
 	            </p>
 	        </div>
 	        <div class="weui-msg__opr-area">
